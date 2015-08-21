6643252da50bca398c03cdf77f80ea1ad4e74376
Brigham <brigham.thompson@gmail.com>
2015-08-21 13:30:18 -0700

    add stylist_id spec for method in Client class

--- spec/clients_spec.rb
+++ spec/clients_spec.rb
@@ -8,4 +8,11 @@ describe(Client) do
       expect(client.name()).to(eq("Sue"))
     end
   end
+
+  describe('#stylist_id') do
+    it("lets you read the stylist ID out") do
+      client = Client.new({:name => "Sue", :stylist_id => 1, :id => nil})
+      expect(client.stylist_id()).to(eq(1))
+    end
+  end
 end
